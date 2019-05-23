940c3d4eab874a1a1ddabc6f9c7df85851ba9c69
Leandro Gallo <X-LGALLO@kforce.com>
2019-05-23 00:00:34 -0300

    net core + angular

---
+++ WebApp/WebApp/Mesa.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApp
+{
+    public class Mesa
+    {
+        public string Modelo { get; set; }
+
+        public string Color { get; set; }
+    }
+
+    public class Inicial
+    {
+        public void main()
+        {
+            var mesa1 = new Mesa
+            {
+                Color = "Marron",
+                Modelo = "Redonda"
+            };
+
+            var mesa2 = new Mesa
+            {
+                Modelo = "Cuadrada",
+                Color = "Amarilla"
+            };
+        }
+        
+        
+    }
+}
